db30680b11322dadc0261fc39bcb0574bf3aa96c
lakanemanuel <lakanemanuel7000@gmail.com>
2025-06-14 03:10:32 +0000

    initialdeployment

---
+++ MainBasic.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+
+//EMANUEL ALARILLA
+
+/**
+ * this is sample of 
+ */
+
+public class MainBasic {
+    public static void main(String[] args) {
+     
+   
+        System.out.println("Hello World");
+         // this is single line comment
+
+         /*
+          * 
+            this is multi line comment
+          */
+    }
+}
+  /**
+      * this is main basic
+      */
+class Basic2
+{
+     
+    MainBasic mb;
+   
+}
+
+class car
+{
+
+    int year;
+    String name;
+    String color;
+}
---
+++ tasksheet111.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+//EMANUEL ALARILLA
+
+ 
+public class tasksheet111 {
+    public static void main(String[] args) {
+     
+        System.out.println("Kumusta Mundo!");
+       
+    }
+}
