7b4def8d5d305312995280ca447731ea27175992
lakanemanuel <lakanemanuel7000@gmail.com>
2025-06-23 03:59:43 +0000

    Activity062325

---
+++ Chapter2Task2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Chapter2Task2 {
+    public static void main(String[] args) {
+      Byte zero = 0;
+      String output = "W" + zero + "w";
+      System.out.println(output);
+  }
+}
