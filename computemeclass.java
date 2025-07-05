a468380eccefc7e0017edaa1ff19998c4ab06d33
lakanemanuel <lakanemanuel7000@gmail.com>
2025-07-05 03:31:56 +0000

    newmethod

--- computemeclass.java
+++ computemeclass.java
@@ -12,4 +12,39 @@ public class computemeclass {
      static int quo(int a, int b) {
     return a / b;
   }
+
+   static int num1(int a) {
+   
+    int sum=0;
+    for(int i=1;i<=a;i++)
+    {
+            sum = sum +i;
+    }
+      
+    return sum;
+  }
+
+   static int num2(int a) {
+   
+    int sum=0;
+    for(int i=1;i<=a;i++)
+    {
+            sum = sum +i;
+    }
+      
+    return sum;
+  }
+
+     static int num3(int a) {
+   
+    int sum=0;
+    for(int i=1;i<=a;i++)
+    {
+            sum = sum +i;
+    }
+      
+    return sum;
+  }
+
+
 }
--- operators.java
+++ operators.java
@@ -4,7 +4,7 @@ public class operators {
   static void computation(int a, int b) {
      computemeclass com = new computemeclass();
 
-     System.out.println("The Sum of two number is " +   com.sum(a,b));
+    System.out.println("The Sum of two number is " +   com.sum(a,b));
     System.out.println("The Diff of two number is " +   com.diff(a,b));
     System.out.println("The Product of two number is " +   com.pro(a,b));
 
@@ -13,18 +13,30 @@ public class operators {
     else
        System.out.println("invalid input data");
     
-  
   }
+
+  static void nums(int a, int b, int c) {
+     computemeclass com = new computemeclass();
+   
+    System.out.println(  "   " + com.num1(a) + "  " + com.num1(b) + "  " + com.num1(c));
+   
+  }
+  
   public static void main(String[] args) {
 
      System.out.print("\033[H\033[2J");
     System.out.flush();
 
-    int num1 = 14;
-    int num2 = 7;
+    int num1 = 3;
+    int num2 = 6;
+    int num3 = 4;
 
-    computation(num1,num2);
-    
+   // computation(num1,num2);
+    nums(num1,num2,num3);
+
+   
+
+
   }
 }
  
